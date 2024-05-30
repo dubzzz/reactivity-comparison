bc60e0fef50dcec3b2e93c727c6330fcfa0cd804
Nicolas DUBIEN <github@dubien.org>
2024-05-30 19:31:43 +0200

    Ignore set when deeply equal by default

--- stacks/react-observable/src/observables/usePipe.ts
+++ stacks/react-observable/src/observables/usePipe.ts
@@ -1,10 +1,16 @@
+import { isEqual } from "lodash";
 import { useState } from "react";
 import { BehaviorSubject } from "rxjs";
+import { readSync } from "./readSync";
 
 export function usePipe<T>(
   initialValue: T
 ): [value: BehaviorSubject<T>, setter: (nextValue: T) => void] {
   const [value] = useState(() => new BehaviorSubject(initialValue));
-  const [setter] = useState(() => (nextValue: T) => value.next(nextValue));
+  const [setter] = useState(() => (nextValue: T) => {
+    if (!isEqual(nextValue, readSync(value))) {
+      value.next(nextValue);
+    }
+  });
   return [value, setter];
 }
